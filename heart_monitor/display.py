1b51ee0840980e0be51f8f2222b3ddd1d2d6dbec
ByoungSul lee <byoungsullee@dhcp-wifi-8021x-155-41-66-27.bu.edu>
2019-02-03 18:19:21 -0500

    updated display.py

--- heart_monitor/display.py
+++ heart_monitor/display.py
@@ -1,10 +1,33 @@
 
 class DisplayHandler(object):
+
     '''
     :developer: Byoungsui Lee && Seunghee Lee
     This class receives numerical data from sensor readers and
     encodes the data for a display technology
+
+    The displayhandler obejct will take in a object containing the vital data.
+    Then it will parse the data into three catagories ( blood oxygen, blood pressure, pulse)
+    Each of these parsed data will be the input for the three display methods.
+
+    :param oxy: uint32_t value of blood oxygen level;
+    returns: void function ( displays blood oxygen level )
+    :raises keyError: raises an exception
+
+    :param systolic: uint32_t value of blood pressure;
+    :param diatolic: uint32_t value of blood pressure;
+    returns: void function ( displays blood pressure level )
+    :raises keyError: raises an exception
+
+    :param pulse: uint32_t value of blood pulse;
+    returns: void function ( displays pulselevel )
+    :raises keyError: raises an exception
     '''
+    def display_blood_oxygen(oxy):
+    def display_blood_pressure(systolic,diatolic):
+    def display_blood_pulse(pulse):
+
+
     pass
 
 
