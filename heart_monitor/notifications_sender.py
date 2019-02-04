ea6484415abc885ad71ce2500d52c381e33367b3
Naelle Zephir <nzephir@bu.edu>
2019-02-03 20:39:06 -0500

    added functions

--- heart_monitor/notifications_sender.py
+++ heart_monitor/notifications_sender.py
@@ -1,24 +1,54 @@
-from common_types import Contact
+from common_types import Contact, Message
 
 
 class NotificationSender(object):
     '''
     :developer Naelle
     Base class that defines shared attributes, methods
+
+    :param message: object_instance of Message class containing message content and urgency
+    :param recipient: string value of recipients number/email from Contact class
+    returns: void function (sends message)
+    :raises keyError: raises an exception
     '''
+    def send_notification(Message, recipient):
     pass
 
 
 # We can define the individual notification senders in more detail later if needed
 class SMSSender(NotificationSender):
+	'''
+    :param message: object_instance of Contact class
+    returns: string with sms destination number
+    :raises keyError: raises an exception
+    '''
+	def find_number(Contact):
+		number = Contact.get_sms()
+		return number
     pass
 
 
 class EmailSender(NotificationSender):
+	'''
+    :param message: object_instance of Contact class
+    returns: string with email destination
+    :raises keyError: raises an exception
+    '''
+	def find_email(Contact):
+		email = Contact.get_email()
+		return email
     pass
 
 
 class TelegramSender(NotificationSender):
+	'''
+    :param message: object_instance of Contact class;
+    returns: string with telegraph destination number
+    :raises keyError: raises an exception
+    '''
+	def find_number(Contact):
+		number = Contact.get_telegraph()
+		return number
     pass
 
 
